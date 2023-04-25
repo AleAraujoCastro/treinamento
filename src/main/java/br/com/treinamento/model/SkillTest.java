5377f69e26d9fa94999ca5470cf3a714cc48bb88
Alexandre Rodolpho Araújo de Castro <datum.alexandre@tokiomarine.com.br>
2023-04-25 17:09:22 -0300

    Primeira Classe.

--- src/main/java/br/com/treinamento/model/SkillTest.java
+++ src/main/java/br/com/treinamento/model/SkillTest.java
@@ -1,11 +1,16 @@
 package br.com.treinamento.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SkillTest {
 
 	public static void main(String[] args) {
 		
 		
+		List<Integer> lista = Arrays.asList(1,5,8,9,1,4,7,6,6,2,2);
 		
+		lista.stream().forEach(e -> System.out.println(e));
 		
 
 	}
