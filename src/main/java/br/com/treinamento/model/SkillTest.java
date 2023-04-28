dafc3c0c2247231b74338dacb250b15abc7d3267
Alexandre Rodolpho Araújo de Castro <datum.alexandre@tokiomarine.com.br>
2023-04-27 23:03:15 -0300

    "Atualizando a primeira Classe"

--- src/main/java/br/com/treinamento/model/SkillTest.java
+++ src/main/java/br/com/treinamento/model/SkillTest.java
@@ -3,14 +3,22 @@ package br.com.treinamento.model;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
 public class SkillTest {
 
 	public static void main(String[] args) {
 		
 		
-		List<Integer> lista = Arrays.asList(1,5,8,9,1,4,7,6,6,2,2);
+		List<Integer> lista = Arrays.asList(1,5,8,9,1,4,7,6,6,2,2);		
 		
-		lista.stream().forEach(e -> System.out.println(e));
+		lista.stream( )
+		.map(e -> e * 2)
+		.forEach(e -> System.out.println(e));
+		
+		JButton jbutton = new JButton();
+		jbutton.addActionListener(e -> System.out.println("Olá Mundo"));
 		
 
 	}
